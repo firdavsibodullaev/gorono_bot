5cbce4cb675fe763349814ecfd823ab3d6e07f43
Фирдавс Ибодуллаев <ibodullaev.firdavs@loc.tashkent.uz>
2024-05-08 15:55:38 +0500

    fixed

--- app/Jobs/SendPostToBotUsersJob.php
+++ app/Jobs/SendPostToBotUsersJob.php
@@ -86,7 +86,7 @@ class SendPostToBotUsersJob implements ShouldQueue
 
                 $this->sendProgressToCreator($postMessage);
 
-                if ($key % 29 === 0 && $key !== 0) {
+                if ($key % 19 === 0 && $key !== 0) {
                     $this->sleep();
                 }
 
